3433e90700bc9848f97cbbab3ad41ccb48b3a510
Anna Sotnikova <ruceba77@gmail.com>
2022-01-04 20:40:32 +0600

    initial commit

---
+++ src/test/java/ru/netology/rest/MobileBankApiTestV2.java
@@ -0,0 +1,25 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+
+class MobileBankApiTestV2 {
+    @Test
+    void shouldReturnDemoAccounts() {
+        //Given - When - Then
+        //Предусловия
+        given()
+                .baseUri("http://localhost:9999/api/v1")
+                //Выполняемые действия
+                .when()
+                .get("/demo/accounts")
+                //Проверки
+                .then()
+                .statusCode(200)
+                //В качестве альтернативы .header("Content-Type", "application/json; charset=UTF-8")
+                //но специализированные проверки - лучше
+                .contentType(ContentType.JSON);
+    }
+}
